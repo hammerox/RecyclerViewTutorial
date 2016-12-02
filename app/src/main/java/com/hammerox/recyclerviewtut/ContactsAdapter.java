abbb32d07819c19d296980b419311165ced96da4
Mauricio Custodio <mauricio_rc2@hotmail.com>
2016-12-02 14:26:57 -0200

    Adapter created

---
+++ app/src/main/java/com/hammerox/recyclerviewtut/ContactsAdapter.java
@@ -0,0 +1,71 @@
+package com.hammerox.recyclerviewtut;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Mauricio on 02-Dec-16.
+ */
+
+public class ContactsAdapter
+        extends RecyclerView.Adapter<ContactsAdapter.ViewHolder> {
+
+    private List<Contact> mContacts;
+    private Context mContext;
+
+
+    public ContactsAdapter(Context context, List<Contact> contacts) {
+        this.mContext = context;
+        this.mContacts = contacts;
+    }
+
+    public Context getContext() {
+        return mContext;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mContacts.size();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View contactView = inflater.inflate(R.layout.item_contact, parent, false);
+
+        ViewHolder viewHolder = new ViewHolder(contactView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Contact contact = mContacts.get(position);
+
+        holder.mTextView.setText(contact.getName());
+        holder.mButton.setText("Message");
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView mTextView;
+        public Button mButton;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            mTextView = (TextView) itemView.findViewById(R.id.contact_name);
+            mButton = (Button) itemView.findViewById(R.id.message_button);
+        }
+    }
+
+}
