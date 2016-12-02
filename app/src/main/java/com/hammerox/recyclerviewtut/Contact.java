2305780ffc4c52f73c91f4672ace11e244f906f5
Mauricio Custodio <mauricio_rc2@hotmail.com>
2016-12-02 13:25:33 -0200

    Contacts added

---
+++ app/src/main/java/com/hammerox/recyclerviewtut/Contact.java
@@ -0,0 +1,38 @@
+package com.hammerox.recyclerviewtut;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Mauricio on 02-Dec-16.
+ */
+
+public class Contact {
+
+    private String mName;
+    private boolean mOnline;
+
+    public Contact(String name, boolean online) {
+        this.mName = name;
+        this.mOnline = online;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public boolean isOnline() {
+        return mOnline;
+    }
+
+    private static int lastContact = 0;
+
+    public static ArrayList<Contact> createContactList(int numContacts) {
+        ArrayList<Contact> contacts = new ArrayList<>();
+
+        for (int i = 1; i <= numContacts ; i++) {
+            contacts.add(new Contact("Person " + ++lastContact, i <= numContacts/2));
+        }
+
+        return contacts;
+    }
+}
