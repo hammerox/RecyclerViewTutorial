a153a19eff129efee49f7309a15ed2d6c2e3927d
Mauricio Custodio <mauricio_rc2@hotmail.com>
2016-12-03 16:33:18 -0200

    Prepared for more fragments

--- app/src/main/java/com/hammerox/recyclerviewtut/MainActivity.java
+++ app/src/main/java/com/hammerox/recyclerviewtut/MainActivity.java
@@ -1,29 +1,96 @@
 package com.hammerox.recyclerviewtut;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final static String KEY_CURRENT_TAG = "CURRENT_TAG";
     private final static String TAG_FRAG_VERT_LIST = "TAG_FRAG_VERT_LIST";
 
     FragmentVerticalList fragmentVerticalList;
+    String currentTag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Fragment fragment;
+
         if (savedInstanceState == null) {
 
-            fragmentVerticalList = new FragmentVerticalList();
+            createFragments();
+            fragment = fragmentVerticalList;
+            currentTag = TAG_FRAG_VERT_LIST;
 
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.activity_main, fragmentVerticalList, TAG_FRAG_VERT_LIST)
-                    .commit();
         } else {
-            fragmentVerticalList = (FragmentVerticalList) getSupportFragmentManager()
-                    .findFragmentByTag(TAG_FRAG_VERT_LIST);
+
+            recreateFragments();
+            currentTag = savedInstanceState.getString(KEY_CURRENT_TAG);
+            switch (currentTag) {
+                case TAG_FRAG_VERT_LIST:
+                    fragment = fragmentVerticalList;
+                    break;
+                default:
+                    fragment = fragmentVerticalList;
+                    break;
+            }
+
         }
+
+        replaceFragment(fragment, currentTag);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        Fragment fragment;
+
+        switch (id) {
+            case R.id.item_vertical_list:
+                fragment = fragmentVerticalList;
+                currentTag = TAG_FRAG_VERT_LIST;
+                break;
+            default:
+                fragment = fragmentVerticalList;
+                currentTag = TAG_FRAG_VERT_LIST;
+                break;
+        }
+
+        replaceFragment(fragment, currentTag);
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(KEY_CURRENT_TAG, currentTag);
+    }
+
+    public void createFragments() {
+        fragmentVerticalList = new FragmentVerticalList();
+    }
+
+    public void recreateFragments() {
+        fragmentVerticalList = (FragmentVerticalList) getSupportFragmentManager()
+                .findFragmentByTag(TAG_FRAG_VERT_LIST);
+    }
+
+    public void replaceFragment(Fragment fragment, String tag) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.activity_main, fragment, tag)
+                .commit();
     }
 }
